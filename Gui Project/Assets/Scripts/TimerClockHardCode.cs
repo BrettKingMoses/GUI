3c2da2d188fbe39dab6b695e0a525a46cf6105d6
BrettKingMoses <brettking96@outlook.com>
2017-09-01 15:49:21 +1000

    untracked shit

---
+++ Gui Project/Assets/Scripts/TimerClockHardCode.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerClockHardCode : MonoBehaviour
+{
+    public float timer;
+    public string clockTime;
+    public Text timeText;
+    void Start()
+    {
+        Time.timeScale = 1;
+    }
+
+    void LateUpdate()
+    {
+        if (timer < 0)
+        {
+            timer = 0;
+        }
+
+    }
+    void Update()
+    {
+        int mins = Mathf.FloorToInt(timer / 60);
+        int secs = Mathf.FloorToInt(timer - mins * 60);
+
+        clockTime = string.Format("{0:0}:{1:00}", mins, secs);
+        timeText.text = "count: " + clockTime;
+        if (timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+    }
+}
