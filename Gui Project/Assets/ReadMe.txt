80c8d3c19238cb08ede99247fa118f849cc4a6c7
BrettKingMoses <brettking96@outlook.com>
2017-09-08 16:03:23 +1000

    added alpha for assessment

---
+++ Gui Project/Assets/ReadMe.txt
@@ -0,0 +1,11 @@
+Assignment Planning
+Art Design will be fantasy
+Lots of snow with a warrior on the right side with a Maul, viking themed, lightly armoured, glowing eyes, storm clouds in background braided white hair and extremely pale complexion
+options menu moves to a second area where the Gui is on the right and a new warrior stands on the left, in a jungle with furs, dark skin a claymore and a more relaxed demeanor, large beard and mowhawk with dreads down his back/side
+/* story for both characters */ /*i keep my best ideas protected, these stories stay on my home pc*/
+everything on the left, art stuff on the right and middle, reverse for options menu
+basic fps keybinds and controller button binds
+ability to save and load
+resolutions
+volume and brightness
+fullscreen with windowed option
---
+++ Gui Project/Assets/Scripts/AssignmentGUIBeta.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AssignmentGUIBeta : MonoBehaviour
+{
+    [Header("Bools")]
+    public bool showOptions;
+    public bool fullScreenToggle;
+    public bool muteToggle;
+    public float volumeSlider, holdingVolume;
+    public float brightnessSlider;
+
+    [Header("Resolution and Sreen Elements")]
+    public int index;
+    public int[] resX, resY;
+    public float scrW, scrH;
+
+
+    [Header("References")]
+    public Light dirLight;
+    public AudioSource music;
+    // Use this for initialization
+    void Start()
+    {
+        scrW = Screen.width / 16;
+        scrH = Screen.height / 9;
+        dirLight = GameObject.FindGameObjectWithTag("Sun").GetComponent<Light>();
+        music = GameObject.Find("MenuMusic").GetComponent<AudioSource>();
+        volumeSlider = music.volume;
+        brightnessSlider = dirLight.intensity;
+    }
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+}
